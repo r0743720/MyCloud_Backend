54d4084beea67f6c98123030d2b6d449ae9369a5
r0743720 <w.wielockx@outlook.com>
2026-05-25 16:38:53 +0200

    front end threw a cors error - added it to the config, based on previous schoolprojects

---
+++ src/main/java/com/mycloud/server/config/CorsConfig.java
@@ -0,0 +1,25 @@
+package com.mycloud.server.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(
+                                "http://localhost:3000",
+                                "http://piward.local:8080"
+                        ) // React dev server
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
+                        .allowedHeaders("*");
+            }
+        };
+    }
+}
